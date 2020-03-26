f335b73058280b3a6eb0248d6932a0da1cfa58f6
aulizar24 <18523199@students.uii.ac.id>
2020-03-27 02:02:28 +0700

    Time-Solution

--- Assets/Bola.cs
+++ Assets/Bola.cs
@@ -18,7 +18,13 @@ public class Bola : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D other){
       if(other.collider.name=="TembokKanan"||other.collider.name=="TembokKiri"){
-        GetComponent<Transform>().position = new Vector2(0,0);
+        StartCoroutine(jeda());
       }
+    }
+    IEnumerator jeda(){
+      sesuatu.velocity = Vector2.zero; // kecepatan 0
+      sesuatu.GetComponent<Transform>().position = new Vector2(0,0);
+      yield return new WaitForSeconds(1);
+      sesuatu.velocity = new Vector2(-1,-1)*speed;   
     }
 }
